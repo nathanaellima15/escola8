b5efd8af214e9d0de8d97d039e394a7bb0061847
Nathanael Lima <nathanaellima15@gmail.com>
2023-06-13 20:15:53 -0300

    entit Aluno8

--- src/main/java/com/mycompany/escola8/entits/Aluno8.java
+++ src/main/java/com/mycompany/escola8/entits/Aluno8.java
@@ -4,10 +4,56 @@
  */
 package com.mycompany.escola8.entits;
 
+import java.util.Date;
+
 /**
  *
  * @author aluno
  */
 public class Aluno8 {
-    
+  private String nome;
+  private char sexo;
+  private int idade;
+  private String matricula;
+  private Date ano;
+
+    public Aluno8(String nome, char sexo, int idade, String matricula, Date ano) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.matricula = matricula;
+        this.ano = ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public Date getAno() {
+        return ano;
+    }
+    public void setAno(Date ano) {
+        this.ano = ano;
+    }
+  
 }
