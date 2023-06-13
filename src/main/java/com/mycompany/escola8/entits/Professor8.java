5a16cb25e3174d053e5b59f042de4e6f7604d7d9
Nathanael Lima <nathanaellima15@gmail.com>
2023-06-13 20:29:00 -0300

    add entit Professor8

---
+++ src/main/java/com/mycompany/escola8/entits/Professor8.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.escola8.entits;
+
+/**
+ *
+ * @author aluno
+ */
+public class Professor8 {
+  private String nome;
+  private char sexo;
+  private int idade;
+  private String cpf;
+
+    public Professor8(String nome, char sexo, int idade, String cpf) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+  
+}
